9002563e2796f80c116629cc9c0b011bd7118ff8
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-08-13 09:28:30 +0700

    update HW

--- day3/Homework_Day3/anime_search.py
+++ day3/Homework_Day3/anime_search.py
@@ -7,16 +7,48 @@ import httpx
 from rich import print
 from rich.progress import track
 
-anime_data = {}
-anime_name = input("Enter the name of the anime: ")
-url = f"https://kitsu.io/api/edge//anime?filter[text]={anime_name}"
-response = httpx.get(url)
-data = response.json()
-for i in track(range(len(data["data"])), description="Fetching data..."):
-    anime_data[i] = {
-        "Anime Name": data["data"][i]["attributes"]["canonicalTitle"],
-        "Anime Type": data["data"][i]["attributes"]["showType"],
-        "Anime Rating": data["data"][i]["attributes"]["averageRating"],
-        "Anime Age Rating": data["data"][i]["attributes"]["ageRating"],
-    }
-print(anime_data)
+
+
+def get_anime_data(anime_name) -> list:
+    anime_data = {}
+    i: int = 0
+    url = f"https://kitsu.io/api/edge//anime?filter[text]={anime_name}"
+    response = httpx.get(url)
+    data = response.json()
+    for i in range(len(data["data"])):
+        anime_data[i] = {
+            "Anime Name": data["data"][i]["attributes"]["canonicalTitle"],
+            "Anime Type": data["data"][i]["attributes"]["showType"],
+            "Anime Rating": data["data"][i]["attributes"]["averageRating"],
+            "Anime Age Rating": data["data"][i]["attributes"]["ageRating"],
+            "Description": data["data"][i]["attributes"]["synopsis"],
+        }
+    return anime_data
+
+
+def main():
+    anime_data: list = {}
+    i: int = 0
+    print("Welcome to Anime Search")
+    try:
+        anime_name = input("Enter the name of the anime: ")
+    except Exception as e:
+        print("Error: ", e)
+        exit()
+    anime_data = get_anime_data(anime_name)
+    # print(anime_data)
+
+    for i in anime_data:
+        print(f"Anime {i+1} :")
+        print("Anime Name: ", anime_data[i]["Anime Name"])
+        print("Anime Type: ", anime_data[i]["Anime Type"])
+        print("Anime Rating: ", anime_data[i]["Anime Rating"])
+        print("Anime Age Rating: ", anime_data[i]["Anime Age Rating"])
+        print("Description: ", anime_data[i]["Description"])
+        print("\n-----------------------------------------------\n")
+    print("\n")
+    print("--------------------- End ------------------------")
+    print("\n")
+
+if __name__ == "__main__":
+    main()
